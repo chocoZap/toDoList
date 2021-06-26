a60b04db6838be4f11efb39bd513daede31df893
福飯　雄太 <yuta@fukuhanyuutanoMacBook-Air.local>
2021-06-27 03:44:11 +0900

    first commit

---
+++ front/src/router/index.js
@@ -0,0 +1,56 @@
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+import Home from '../views/Home.vue'
+// コンポーネントをインポート
+import TaskIndex from "../views/example/index";
+import TaskShow from "../views/example/show";
+import TaskCreate from "../views/example/create";
+import TaskEdit from "../views/example/edit";
+
+Vue.use(VueRouter)
+
+const routes = [
+  {
+    path: '/',
+    name: 'Home',
+    component: Home
+  },
+  {
+    path: '/about',
+    name: 'About',
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+  },
+  {
+    path: '/tasks',
+    name: 'task.list',
+    component: TaskIndex
+  },
+  {
+    path: '/tasks/create',
+    name: 'task.create',
+    component: TaskCreate
+  },
+  {
+    path: '/tasks/:taskId',
+    name: 'task.show',
+    component: TaskShow,
+    props: true
+  },
+  {
+    path: '/tasks/:taskId/edit',
+    name: 'task.edit',
+    component: TaskEdit,
+    props: true
+  },
+]
+
+const router = new VueRouter({
+  mode: 'history',
+  base: process.env.BASE_URL,
+  routes
+})
+
+export default router
