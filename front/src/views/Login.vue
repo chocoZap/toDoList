59dc53cc1279f78313e2c2d50dedd4b8f4f05a4b
浅井孝文 <asaitakafumi@asaitakafuminoMacBook-Air.local>
2021-06-27 16:24:16 +0900

    修正しました

--- front/src/views/Login.vue
+++ front/src/views/Login.vue
@@ -1,19 +1,25 @@
 <template lang="pug">
   v-app
-    v-card(width="400px").mx-auto.mt-5
+    v-card.mx-auto.mt-5(width="400px")
       v-card-title
         hi.display-1 ログイン
       v-card-text
         v-form
-          v-text-field(prepend-icon="mdi-account-circle")(label="ユーザー名")
-          v-text-field(v-bind:type="showPassword ? 'text' : 'password'")(prepend-icon="mdi-lock")(v-bind:append-icon="showPassword ? 'mdi-eye' : 'mdi-eye-off'")(label="パスワード")(@click:append="showPassword = !showPassword")
+          v-text-field(prepend-icon="mdi-account-circle" label="ユーザー名")
+          v-text-field(
+            :type="showPassword ? 'text' : 'password'"
+            prepend-icon="mdi-lock"
+            :append-icon="showPassword ? 'mdi-eye' : 'mdi-eye-off'"
+            label="パスワード"
+            @click:append="showPassword = !showPassword"
+          )
           v-card-actions
             v-btn.info ログイン
 
 </template>
 <script>
   export default {
-    name: 'App',
+    name: 'Login',
     data: () => ({
       showPassword : false
     })
